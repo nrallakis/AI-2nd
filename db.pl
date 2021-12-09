82eea6c857ea62bbee81e18f4c1d539410045858
Nicholas Rallakis <nrallakis4@gmail.com>
2021-12-09 18:46:23 +0200

    Add literals

--- db.pl
+++ db.pl
@@ -1,1 +1,3 @@
 find_sim_1(X, Y):- genre(X, G1), genre(Y, G1), X \= Y.
+find_sim_2(X, Y):- find_sim_1(X, Y), vote_average(X, VoteX), number_string(VX,VoteX),  vote_average(Y, VoteY), number_string(VY,VoteY), (VoteX) > (VoteY).
+vote(X, Y):- vote_average(X, VoteX).
